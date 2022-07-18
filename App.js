0e2348abc1a29e2bdb95e540617a14019e549e2a
Claudia Elizabeth Arcos Obando <carcos@itsqmet.edu.ec>
2022-07-17 22:38:24 -0500

    archivos base

---
+++ App.js
@@ -0,0 +1,7 @@
+import Navigation from './app/navigation/Navigation';
+
+export default function App() {
+  return (
+       <Navigation /> 
+  );
+}
---
+++ app/screens/Register.js
@@ -0,0 +1,91 @@
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Image, TextInput, Pressable } from "react-native";
+
+export default function Register(props) {
+    const {navigate} = props.navigation;
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+  
+    const registrar = () => {
+        console.log(props);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.tinyLogo}
+                source={require('../../assets/logo.png')}
+            />
+            <Text style={styles.title}>Compra Fácil y Rápido</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Correo Electrónico"
+                onChangeText={(value) => setEmail(value)}
+                value={email}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Contraseña"
+                onChangeText={(value) => setPassword(value)}
+                secureTextEntry={true}
+                value={password}
+            />
+            <Pressable
+                onPress={registrar}
+                style={styles.button}
+            >
+                <Text style={styles.textButton}>Registrar</Text>
+            </Pressable>
+            <Text onPress={() => navigate("Login")} 
+            style={styles.link}>¿Ya tienes una cuenta?</Text>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        //justifyContent: 'center',
+    },
+    tinyLogo: {
+        width: 250,
+        height: 125,
+        marginTop: 100,
+        marginBottom: 30,
+    },
+    title:{
+        marginBottom: 50,
+    },
+    input: {
+        marginTop: 20,
+        borderWidth: 1,
+        width: 300,
+        height: 40,
+        borderRadius: 20,
+        borderColor: "#02CCFF",
+        padding: 10,
+    },
+    button: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: "#02CCFF",
+        borderRadius: 7,
+        width: 300,
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textButton: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    link: {
+        marginTop:20,
+        color:"#02CCFF",
+        fontWeight: "bold",
+    }
+});
